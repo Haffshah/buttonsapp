18f3ec52ab7b1e7046c9ca4116cf96ff89663d90
Harsh M Shah <shahharsh7786@gmail.com>
2021-09-27 18:58:30 +0530

    DropDown Menu

--- lib/MenuPage.dart
+++ lib/MenuPage.dart
@@ -9,11 +9,44 @@ class DropMenu extends StatefulWidget {
 }
 
 class _DropMenuState extends State<DropMenu> {
+  String dropdownValue = 'Low';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(title: Text('Custom Dropdown Menu'),),
+      appBar: AppBar(
+        title: Text('Custom Dropdown Menu'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Normal Use of Menu Button'),
+          ),
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: const Icon(Icons.arrow_drop_down),
+            iconSize: 24,
+            elevation: 16,
+            style: const TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
+            items: <String>['Low', 'Medium', 'High']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
     );
   }
 }
